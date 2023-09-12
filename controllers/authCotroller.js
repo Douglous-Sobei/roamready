9444ececa6bfe00fd06998e6bc2cc5c6bbe15a07
Douglous Sobei <douglousmangoyi@gmail.com>
2023-09-13 01:49:32 +0300

    Password Reset Functionality

--- controllers/authCotroller.js
+++ controllers/authCotroller.js
@@ -98,3 +98,18 @@ exports.restrictTo =
     }
     next();
   };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // Get user based on Posted email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError('There is no user with the email address.', 404));
+  }
+  // Generate the random  reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save();
+
+  // Send it to user's email
+});
+
+exports.resetPassword = (req, res, next) => {};
