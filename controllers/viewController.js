729430c6ca54b262e9a485a28f41842a5b3c57e5
Douglous Sobei <douglousmangoyi@gmail.com>
2023-09-24 01:04:00 +0300

    Building the tour page part one

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -14,8 +14,17 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  // Get the data for the requested tour including reviews and guides
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
+  // Build template
+  // Render template using requested data from step 1
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour,
   });
-};
+});
