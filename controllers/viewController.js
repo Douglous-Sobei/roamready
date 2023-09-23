e4cdb25487aecc6661dc305e41fe3028a7cf1ba4
Douglous Sobei <douglousmangoyi@gmail.com>
2023-09-23 23:33:33 +0300

    Building the Tour overview

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,18 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModels');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  // Get Tour data from collection
+  const tours = await Tour.find();
+  // Build template
+
+  // Render template using tour data from step 1
+
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
