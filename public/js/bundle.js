f9b696899edebc5806750f303a32cb970c5209f6
Douglous Sobei <douglousmangoyi@gmail.com>
2023-09-28 21:34:59 +0300

    Image Uploads Using Mylter:Users

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12370,7 +12370,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42857" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37305" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
