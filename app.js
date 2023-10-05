dbf418260967c877bee87f949faa5d9d7c49d0b6
Douglous Sobei <douglousmangoyi@gmail.com>
2023-10-05 22:30:38 +0300

    integrating stripe into backend

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
@@ -27,16 +28,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Set security HTTP headers
 app.use(helmet());
 
-// Development Logging
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-// Limit requests from same Api
+// Limit requests from same API
 const limiter = rateLimit({
-  max: 120,
+  max: 100,
   windowMs: 60 * 60 * 1000,
-  message: 'Too many request from this IP, please try again in an hour',
+  message: 'Too many requests from this IP, please try again in an hour!',
 });
 app.use('/api', limiter);
 
@@ -48,27 +49,27 @@ app.use(cookieParser());
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
 
-// Data sanitization against xss
+// Data sanitization against XSS
 app.use(xss());
 
-// Prevent parameter polution
+// Prevent parameter pollution
 app.use(
   hpp({
     whitelist: [
       'duration',
-      'ratingsQantity',
+      'ratingsQuantity',
       'ratingsAverage',
       'maxGroupSize',
-      'price',
       'difficulty',
+      'price',
     ],
   }),
 );
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(req.cookies);
-
+  // console.log(req.cookies);
   next();
 });
 
@@ -77,6 +78,7 @@ app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
