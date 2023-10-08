c55ee38a17fa756edd48da3ad2528e34c7c6a329
Douglous Sobei <douglousmangoyi@gmail.com>
2023-10-08 15:54:24 +0300

    Creating new booking on checkout success

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,5 +1,6 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModels');
+const Booking = require('../models/bookingModels');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
@@ -11,7 +12,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -25,7 +28,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
             description: tour.summary,
             images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
           },
-          unit_amount: tour.price * 100,
+          unit_amount: tour.price * 100, // Price in cents
         },
         quantity: 1,
       },
@@ -37,4 +40,12 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     status: 'success',
     session,
   });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  // This only temporary, because it is insecure
+  const { tour, user, price } = req.query;
+  if (!tour && !user && !price) return next();
+  await Booking.create({ tour, user, price });
+  res.redirect(req.originalUrl.split('?')[0]);
 });
