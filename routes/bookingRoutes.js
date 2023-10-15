867febdd8a08a8891556e6410bba8866344c811e
Douglous Sobei <douglousmangoyi@gmail.com>
2023-10-15 09:16:03 +0300

    Rendering a users booked tours

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,25 +4,10 @@ const authController = require('../controllers/authCotroller');
 
 const router = express.Router();
 
-// router.use(authController.protect);
-
 router.get(
   '/checkout-session/:tourId',
   authController.protect,
   bookingController.getCheckoutSession,
 );
-
-// router.use(authController.restrictTo('admin', 'lead-guide'));
-
-// router
-//   .route('/')
-//   .get(bookingController.getAllBookings)
-//   .post(bookingController.createBooking);
-
-// router
-//   .route('/:id')
-//   .get(bookingController.getBooking)
-//   .patch(bookingController.updateBooking)
-//   .delete(bookingController.deleteBooking);
 
 module.exports = router;
