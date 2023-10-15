48bc34f665d971858e54731cffd8d68f632aa954
Douglous Sobei <douglousmangoyi@gmail.com>
2023-10-16 00:05:28 +0300

    Implemented webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = (env, argv) => {
+  const isDevelopment = argv.mode === 'development';
+
+  return {
+    entry: './public/js/index.js',
+    output: {
+      path: path.resolve(__dirname, 'public/js'),
+      filename: isDevelopment ? 'bundle.js' : 'bundle.[contenthash].min.js',
+    },
+    mode: isDevelopment ? 'development' : 'production', // Set mode here
+    devtool: isDevelopment ? 'source-map' : false,
+    optimization: {
+      minimizer: [new TerserPlugin()],
+      splitChunks: {
+        chunks: 'all',
+      },
+    },
+  };
+};
